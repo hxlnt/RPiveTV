4f9115ea683a83cd003464b47248853cbb142e0a
Rachel Weil <rae@Rachels-iMac.local>
2017-08-05 04:12:05 -0500

    Adding search for testing purposes

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -10,23 +10,29 @@ module.exports = function (context, myQueueItem) {
 
     const query = new azure.TableQuery().where('VTT eq ?', '');
 
+    Vindexer.search({
+        // Optional
+        language: 'English',
+        searchInPublicAccount: false
+    }).then( function(result){ context.log (`results: ${result.body}`) } );
+
     tableSvc.queryEntities('bluescreenofdeath', query, null, function (error, result, response) {
         if (!error) {
             context.log(result.entries)
-            for (i = 0; i < result.entries.length; i++) {
-                context.log(`part 1... loop through ${result.entries.length} times`)
-                Vindexer.uploadVideo(result.entries[i].url._, {
-                    name: result.entries[i].RowKey._ + result.entries[i].RowKey._,
-                    privacy: 'Private',
-                    language: 'English'
-                })
-                    .then(Vindexer.getVttUrl(result.body))
-                    .then(tableSvc.mergeEntity('bluescreenofdeath', task, { echoContent: true }, function (error, result, response) {
-                        var updatevtt = { VTT: result.body };
-                        context.log(`vtt updated: ${result.body}`);
-                    })
-                );
-            }
+            // for (i = 0; i < result.entries.length; i++) {
+            //     context.log(`part 1... loop through ${result.entries.length} times`)
+            //     Vindexer.uploadVideo(result.entries[i].url._, {
+            //         name: result.entries[i].RowKey._ + result.entries[i].RowKey._,
+            //         privacy: 'Private',
+            //         language: 'English'
+            //    })
+                //     .then(Vindexer.getVttUrl(result.body))
+                //     .then(tableSvc.mergeEntity('bluescreenofdeath', task, { echoContent: true }, function (error, result, response) {
+                //         var updatevtt = { VTT: result.body };
+                //         context.log(`vtt updated: ${result.body}`);
+                //     })
+                // );
+            // }
         }
         else { context.log(`error`); }
     });
