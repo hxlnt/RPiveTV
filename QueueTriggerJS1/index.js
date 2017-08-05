fb49841512587a97891041914830d2260035bd85
Rachel Simone Weil <partytimehexcellent@gmail.com>
2017-08-05 03:31:10 -0500

    Update index.js

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -1,4 +1,19 @@
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    
+    
+    //Set up Azure Storage credentials
+    var azure = require('azure-storage')
+    var tableSvc = azure.createTableService(process.env.azurestoragename, process.env.azurestoragekey)
+
+    var query = new azure.TableQuery().where('VTT eq ?', '');
+    
+    tableSvc.queryEntities('bluescreenofdeath', query, null, function(error, result, response) {
+        if(!error) {
+            console.log(result.entries)
+        }
+        else { console.log(`error`); }
+    });
+    
     context.done();
 };
