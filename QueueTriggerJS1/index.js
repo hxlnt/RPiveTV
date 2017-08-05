9665df8f72892bd31423275768aa276263eebd3a
Rachel Weil <rae@Rachels-iMac.local>
2017-08-05 03:59:20 -0500

    Added missing )

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -1,7 +1,7 @@
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    
-    
+
+
     //Set up Azure Storage credentials
     const azure = require('azure-storage');
     const tableSvc = azure.createTableService(process.env.azurestoragename, process.env.azurestoragekey);
@@ -9,25 +9,26 @@ module.exports = function (context, myQueueItem) {
     const Vindexer = new vindexer(process.env.videoindexerkey);
 
     const query = new azure.TableQuery().where('VTT eq ?', '');
-    
-    tableSvc.queryEntities('bluescreenofdeath', query, null, function(error, result, response) {
-        if(!error) {
+
+    tableSvc.queryEntities('bluescreenofdeath', query, null, function (error, result, response) {
+        if (!error) {
             context.log(result.entries)
-            for (i=0; i<result.entries.length; i++) {
+            for (i = 0; i < result.entries.length; i++) {
                 Vindexer.uploadVideo(result.entries[i].url._, {
                     name: result.entries[i].RowKey._ + result.entries[i].RowKey._,
-                    privacy: 'Private', 
+                    privacy: 'Private',
                     language: 'English'
                 })
-                .then(Vindexer.getVttUrl(result.body))
-                .then(tableSvc.mergeEntity('bluescreenofdeath', task, {echoContent: true}, function (error, result, response) {
-                    var updatevtt = { VTT: result.body };
-                    context.log(`vtt updated: ${result.body});
-                });
+                    .then(Vindexer.getVttUrl(result.body))
+                    .then(tableSvc.mergeEntity('bluescreenofdeath', task, { echoContent: true }, function (error, result, response) {
+                        var updatevtt = { VTT: result.body };
+                        context.log(`vtt updated: ${result.body}`);
+                    })
+                );
             }
         }
         else { context.log(`error`); }
     });
-    
+
     context.done();
 };
