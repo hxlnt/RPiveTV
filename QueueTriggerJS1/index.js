ce212a46b2190007db2171578a9ab6499b53c7c3
Rachel Weil <rae@Rachels-iMac.local>
2017-08-06 12:07:33 -0500

    adding back vtt url update

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -38,7 +38,7 @@ module.exports = function (context, myQueueItem) {
                     language: 'English'
                 })
                     .then(Vindexer.getVttUrl(result.body))
-                    .then(tableSvc.mergeEntity('bluescreenofdeath', { PartitionKey: currPK, RowKey: currRK, VTT: "test" }, { echoContent: true }, function (error, result, response) {
+                    .then(tableSvc.mergeEntity('bluescreenofdeath', { PartitionKey: currPK, RowKey: currRK, VTT: result.body }, { echoContent: true }, function (error, result, response) {
                         context.log(`vtt updated`);
                     })
                 );
